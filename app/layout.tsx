db08057737ab936494328486788889b2743012f4
Algogi-Technologies <jayant@algogi.com>
2025-06-06 18:52:51 +0530

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { Providers } from './providers';
 
@@ -14,6 +15,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-49ZLWB78C6"></Script>
+			<Script id="gtag-S">
+				{`window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-49ZLWB78C6');`}
+			</Script>
 			<body className={inter.className}>
 				<Providers attribute="class" defaultTheme="system" enableSystem>
 					{children}
